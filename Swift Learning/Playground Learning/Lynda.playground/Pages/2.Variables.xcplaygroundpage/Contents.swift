6cbf4743fde9f01b74340a050c4ab8be72567848
jamaal51 <jsedayao@gmail.com>
2016-01-29 12:02:18 -0500

    lynda practice

---
+++ Swift Learning/Playground Learning/Lynda.playground/Pages/2.Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+let aCharacter: Character = "a"
+
+let bCharacter = "b" as Character
+
+let uWithUmlaut: Character = "\u{75}\u{308}"
