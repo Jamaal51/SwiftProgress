c8d74e46dcf5b2085060cdae56dfa6819d5148c8
jamaal51 <jsedayao@gmail.com>
2016-01-31 10:37:08 -0500

    class update

--- Swift Learning/Class/Sets_Playground.playground/Contents.swift
+++ Swift Learning/Class/Sets_Playground.playground/Contents.swift
@@ -28,3 +28,49 @@ allColors.isSubsetOf(myFaveColors)
 secondaryColors.isDisjointWith(primaryColors)
 secondaryColors.intersect(primaryColors).isEmpty
 myFaveColors.isDisjointWith(primaryColors)
+
+//Why use Sets? check for words - SpellCheck
+// add or remove custom words
+
+//Protocol
+
+protocol SetProtocol {
+    typealias ItemType
+    mutating func add(item: ItemType)
+    mutating func remove(item: ItemType)
+    func contains(item: ItemType) -> Bool
+    
+}
+
+struct ArraySet<T: Equatable>: SetProtocol {
+    var elements = [T]()
+    typealias ItemType = T
+    mutating func add(item: T) {
+        elements.append(item)
+    }
+    mutating func remove(item: T) {
+        elements.removeAtIndex(elements.indexOf(item)!)
+    }
+    func contains(item: T) -> Bool {
+    return elements.contains(item)
+    }
+    //CustomStringConvertible
+    var description: String{
+        return "{\(elements)}"
+    }
+}
+
+var colors = ArraySet<String>()
+colors.add("blue")
+colors.add("green")
+colors.add("orange")
+
+colors.remove("blue")
+colors.contains("blue")
+
+
+
+
+
+
+
