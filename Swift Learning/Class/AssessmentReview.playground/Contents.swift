5cf9b74222267dbb6cc42642d5632a6723debe2b
jamaal51 <jsedayao@gmail.com>
2016-02-05 19:28:09 -0500

    update review

--- Swift Learning/Class/AssessmentReview.playground/Contents.swift
+++ Swift Learning/Class/AssessmentReview.playground/Contents.swift
@@ -73,20 +73,20 @@ for var x in 0..<TwoDimensionalArray.count{
 
 //Print out all the elements in a spiral.
 
-func printSpiral(twoDArray:[[Int]]){
-    if twoDArray.count == 0{
-        return
-    }
-    //intitalize indexes top, bottom, left, right
-    var top = 0
-    var bottom = twoDArray.count
-    var left = 0
-    var right = twoDArray.count
-    
-    print(right)
-}
+//func printSpiral(twoDArray:[[Int]]){
+//    if twoDArray.count == 0{
+//        return
+//    }
+//    //intitalize indexes top, bottom, left, right
+//    var top = 0
+//    var bottom = twoDArray.count
+//    var left = 0
+//    var right = twoDArray.count
+//    
+//    print(right)
+//}
 
-printSpiral(TwoDimensionalArray)
+//printSpiral(TwoDimensionalArray)
 
 
 
@@ -96,6 +96,128 @@ func checkSudokuBoard(sudokuBoard:[[Int]]) -> Bool {
     
     return true
 }
+
+//Big O
+
+// O(1) < O(log(n)) < O(n) < O(n log(n)) < O (n^2) < O (2^n) < O(n!)
+
+// Data Structures
+
+/*
+Arrays
+Access - O(1), Search/Delete/Insert - O(n)
+
+Lists
+Access/Search - O(n), Insert/Delete - O(1)
+
+Stacks
+LIFO
+Access/Search - O(n), Insert/Delete - O(1)
+
+Queues
+FIFO
+
+Sets 
+use cases: spellcheck, blacklists
+add - O(1), O(n)*, remove - O(n), check- O(n), union- O(n)/O(nm)*, intersect - O(nm), difference- O(nm) subset- O(nm)
+*with dupe checking
+
+Hashmaps
+rule 1: x==y, h(x)==h(y) ALWAYS, if not Invalid, ie use of NSDate
+rule 2: x!=y, h(x)!=h(y), Usually, some values have same hash - collision
+bucketing - creating a linked list at each index
+open addressing - adding to an empty index
+
+get hash value <- foo.hashValue % capacity(array.length)
+add/remove/contains - O(n) , union/intersect/difference/subset- O(nm)
+
+Trees
+methods - O(log(n))
+
+Graphs
+
+SORTING ALGORITHMS
+
+Quicksort - Average O(n log(n)), Worst O(n^2)
+
+Merge - Worst O(n log(n))
+
+Bubble - Worst O(n^2)
+
+Insertion - Worst O(n^2)
+
+Selection - Worst O(n^2)
+*/
+
+//Bubble Sort Implementation
+
+func BubbleSort(inout unsortedArray:[Int]) {
+    //var temp: Int;
+    var sorted = false
+    
+    while (!sorted){
+    sorted = true
+    for i in 0..<unsortedArray.count-1{
+        if unsortedArray[i+1] < unsortedArray[i]{
+            let temp = unsortedArray[i+1]
+            unsortedArray[i+1] = unsortedArray[i]
+            unsortedArray[i] = temp
+            sorted = false
+            }
+        }
+        if sorted{
+            break
+        }
+    }
+}
+
+var array:[Int] = [5,9,12,44,1,0,4,2,100,45]
+
+BubbleSort(&array)
+
+
+// Insertion Sort implementation
+
+func InsertionSort(inout numberList:[Int]){
+    var x, y, key : Int
+    for (x = 0; x < numberList.count; x++) {
+        key = numberList[x]
+        for (y = x; y > -1; y--) {
+            if (key < numberList[y]) {
+                numberList.removeAtIndex(y + 1)
+                numberList.insert(key, atIndex: y)
+            }
+        }
+    }
+}
+
+InsertionSort(&array)
+
+func InsertionSortTwo(inout numberList:[Int]){
+    var y, key: Int // create variables
+    for x in 0..<numberList.count{
+        key = numberList[x]
+        for (y=x; y >= 0; y--){
+            if (key < numberList[y]){
+                numberList.removeAtIndex(y+1)
+                numberList.insert(key, atIndex: y)
+            }
+        }
+    }
+}
+
+InsertionSortTwo(&array)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
