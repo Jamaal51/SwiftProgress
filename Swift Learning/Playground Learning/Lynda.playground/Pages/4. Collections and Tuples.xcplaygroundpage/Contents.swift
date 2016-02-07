35cd1a884e08a25725c5145fa5d5e1dc837235f0
jamaal51 <jsedayao@gmail.com>
2016-02-07 16:45:59 -0500

    update

--- Swift Learning/Playground Learning/Lynda.playground/Pages/4. Collections and Tuples.xcplaygroundpage/Contents.swift
+++ Swift Learning/Playground Learning/Lynda.playground/Pages/4. Collections and Tuples.xcplaygroundpage/Contents.swift
@@ -32,6 +32,134 @@ let arrayOfStrings = [nilString, someString]
 
 var optionalArrayOfStrings: [String]?
 
-var optionalArrayOfOptionalValues: [String?]?
+var  optionalArrayOfOptionalValues: [String?]?
+
+threeStooges.count
+threeStooges.isEmpty
+threeStooges.contains("Moe")
+
+let larry = threeStooges[1]
+
+let twoStooges = threeStooges[0..<2].sort(>)       //slicing an array
+                                                // .sort(<) ascending (>) descending
+let moe = threeStooges.first! //force unwrap
+
+let curly = threeStooges.last!
+
+turnstileCounts[2] = 5
+
+turnstileCounts[0] += 10
+
+var fourStooges = threeStooges
+
+fourStooges.append("Shemp")
+
+let shemp = fourStooges.removeLast()
+
+fourStooges.insert(shemp, atIndex: 0)
+
+var allStooges = fourStooges + ["Ted", "Emil"]
+
+allStooges += ["Joe Besser", "Joe DeRita"]
+
+allStooges.removeRange(4...5)
+
+allStooges.sortInPlace()
+
+//Nesting Arrays
+
+var famousGroupsOfThree = [
+    threeStooges,
+    ["Huey","Dewey","Louie"],
+    ["Athos","Porthos","Aramis"],
+    ["Jack","Chrissy","Jane"]
+]
+
+let threeMusketeers = famousGroupsOfThree[2]
+let jack = famousGroupsOfThree[3][0]
+
+famousGroupsOfThree[3][2] = "Janet"
+
+//print(famousGroupsOfThree.flatMap({ $0 })) //flatmap returns array of concatenated results, {$0} represents all of values
+
+
+//Defining Dictionaries
+
+var stockPrices = ["AAPL": 110.37, "GOOG": 606.25, "MSFT": 43.5]
+
+var birthYears: [String: Int] = [:]
+
+var raceResults = Dictionary<Int, String>()
+
+let tourDeFranceResults: [Int: String]
+tourDeFranceResults = [
+    1: "Chris Froome",
+    2: "Nairo Quintana",
+    3: "Alejandro Valverde"
+]
+//dictionary keys must not be optional and must conform to the hashable protocol
+//each key must be a unique value ie. can't use a Bool to represent a key or dictionary will have only two values
+//dictionary values should not be optional
+
+var ages = Dictionary<String, Int>()
+
+stockPrices.count
+
+ages.isEmpty
+
+Array(stockPrices.keys)
+Array(stockPrices.values)
+
+let appleStockPrice = stockPrices["AAPL"]!
+
+//modify dictionaries
+
+//add
+stockPrices["AMZN"] = 504.72
+
+//edit
+stockPrices["AAPL"] = 114.45
+
+//remove from dict just set equal to nil
+stockPrices["AMZN"] = nil
+
+//print(stockPrices)
+
+stockPrices.removeAll()
+
+stockPrices["DMI"] = 17.25
+
+let dunderMifflinStockPrice = stockPrices.removeValueForKey("DMI")
+
+// removeValueForKey returns an optional since key might not have a value
+
+//Nesting Dictionaries
+
+var bostonMarathonResults: [Int : [String:String]] = [
+    1:[
+        "name": "Desisa",
+        "country": "ETH",
+        "finishingTime": "2:09:17"
+    ],
+    2:[
+        "name": "Kris",
+        "country": "BOL",
+        "finishingTime": "2:09:50"
+    ]
+]
+
+let firstPlaceName = bostonMarathonResults[1]?["name"]  //Optional Chaining
+
+//Defining Sets
+
+
+
+
+
+
+
+
+
 
 //: [Next](@next)
+ 
