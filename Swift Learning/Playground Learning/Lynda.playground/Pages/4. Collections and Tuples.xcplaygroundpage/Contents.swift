61da98df63b7229915c42c859d39fb2776eb4c39
jamaal51 <jsedayao@gmail.com>
2016-02-10 15:38:47 -0500

    update

--- Swift Learning/Playground Learning/Lynda.playground/Pages/4. Collections and Tuples.xcplaygroundpage/Contents.swift
+++ Swift Learning/Playground Learning/Lynda.playground/Pages/4. Collections and Tuples.xcplaygroundpage/Contents.swift
@@ -194,13 +194,35 @@ var numbers: (Int, Float, Double) = (1, 2.3, 3.4)
 
 var httpStatus: (Int, String?)?
 
-var playerScores: ([Int], firstName: String, lastName:String) //array of ints, string, string
+var playerScores: ([Int], firstName: String, lastName:String?) //array of ints, string, string
 
 playerScores = ([134_000, 128_500, 156_250], "Scott", "Gardner")
 
 let highScore = playerScores.0.maxElement()!
 
 print("\(playerScores.firstName) acheived a high score of \(highScore).")
+
+let (scores, firstName, lastName) = playerScores
+
+let averageScore = scores.reduce(0, combine: +)/scores.count
+
+print("\(firstName) achieved an average score of \(averageScore)")
+
+let (httpStatusCode, _) = httpStatus200 //how to write a tuple decomposed for values that arent needed
+
+//nested tuples
+
+var anotherScore = (100, (firstName: "Scott", lastName: "Gardner"))
+
+let (theScore, name) = anotherScore
+
+name.firstName //can access firstName from the name assigned from anotherScore variable
+
+anotherScore.0 = 98
+
+anotherScore.1.firstName = "Brett"
+
+
 
 //: [Next](@next)
  
