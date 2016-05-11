575480205fbf14ca2f2c48a0419c22689bc9d2fc
Jamaal Sedayao <jsedayao@gmail.com>
2016-05-11 17:24:46 -0400

    added face

---
+++ SwiftLearning/StanfordProjects/FaceItProj/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Jamaal Sedayao on 5/9/16.
+//  Copyright © 2016 Jamaal Sedayao. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+ 
+
+}
+
